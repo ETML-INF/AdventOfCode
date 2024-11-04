da316972b8db6d41580dcd849be23d57cf5e8e04
samumuku <sallakusamuel39@gmail.com>
2024-11-04 15:44:19 +0100

    Jour 1, partie 1 fini

---
+++ SamBas/Day1/Day1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] max3;
+            string[] calories;
+            calories = File.ReadAllLines("../../../input.txt");
+            int[] totalCalories = new int[calories.Length];
+            int j = 0;
+            for (int i = 0; i < calories.Length; i++)
+            {
+                if (calories[i] != "")
+                {
+                    totalCalories[j] += int.Parse(calories[i]);
+                }
+                else
+                {
+                    j++;
+                }
+            }
+            Console.WriteLine(totalCalories.Max());
+            totalCalories.OrderBy();
+            Console.ReadLine();
+        }
+    }
+}
--- SamBas/README.md
+++ SamBas/README.md
@@ -1,3 +1,7 @@
 # Team SamBas
 
 Membres : Bastien & Samuel
+
+# 04.11.2024
+
+Nous avions fini la partie 1 du jour 1, nous sommes dans la partie 2.
